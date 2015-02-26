3a814c48836133cbdc8ad30dd2d773404998df76
Michael Grauer <michael.grauer@kitware.com>
2015-02-26 11:28:38 -0500

    Add API method getDatasetEvents

--- server/__init__.py
+++ server/__init__.py
@@ -16,9 +16,11 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 ###############################################################################
-
+import json
+import bson
 from girder.api import access
 from girder.api.rest import Resource, loadmodel
+from girder.api.rest import RestException
 from girder.api.describe import Description
 from girder.constants import AccessType
 
@@ -77,9 +79,50 @@ class InfoPage(Resource):
         .errorResponse('Write permission denied on the collection.', 403))
 
 
+class DatasetEvents(Resource):
+    @access.public
+    @loadmodel(model='collection', level=AccessType.READ)
+    def getDatasetEvents(self, collection, params):
+        """
+        Get the individual events in a dataset, which are defined as folders
+        having metadata keys (scan_date, subject_id, scan_weight_kg, DOB) and
+        that live under the passed in collection.
+        Handle setting infoPage for any resource that supports them.
+        :param collection: parent collection of sought events.
+        :return resource: the loaded resource document.
+        """
+        model = self.model('folder')
+        # return these metadata keys
+        metadata_keys = ['scan_date', 'subject_id', 'DOB', 'scan_weight_kg']
+        key_d = {'meta.'+key: 1 for key in metadata_keys}
+        # look at folders in this collection with the scan_date metadata key
+        condition_d = {'baseParentId': {'$oid': collection['_id']},
+                       'meta.scan_date': {'$exists': True}}
+        initial = {}
+        reduce = "function (curr, result) {}"
+        finalize = "function (curr, result) {}"
+        try:
+            key = bson.json_util.loads(bson.json_util.dumps(key_d))
+            condition = bson.json_util.loads(bson.json_util.dumps(condition_d))
+        except ValueError:
+            raise RestException('The query parameter must be a JSON object.')
+        document = \
+            model.collection.group(key, condition, initial, reduce, finalize)
+        # TODO filtering, paging, permission checks
+        return document
+    getDatasetEvents.description = (
+        Description('Get datasetEvents for the collection.')
+        .param('id', 'The collection ID', paramType='path')
+        .errorResponse('ID was invalid.')
+        .errorResponse('Read permission denied on the collection.', 403))
+
+
 def load(info):
     infoPage = InfoPage()
     info['apiRoot'].collection.route('GET', (':id', 'infoPage'),
                                      infoPage.getCollectionInfoPage)
     info['apiRoot'].collection.route('PUT', (':id', 'infoPage'),
                                      infoPage.setCollectionInfoPage)
+    datasetEvents = DatasetEvents()
+    info['apiRoot'].collection.route('GET', (':id', 'datasetEvents'),
+                                     datasetEvents.getDatasetEvents)
